2f5e4b211dca4d0cb62cfd6df7bebef8756734f2
Kazkoviy <illyaboiko1807@gmail.com>
2021-12-18 00:09:32 +0200

    basic settings

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { router } from './router'
+import { store } from './store'
+import './assets/styles/main.scss'
 
 createApp(App)
   .use(store)
-  .use(router).mount('#app')
+  .use(router)
+  .mount('#app')
